d1c4bcf0112ccd04a0ef03326902b076e30195dd
Nicholas Frechette <zeno490@gmail.com>
2024-07-06 12:11:13 -0400

    chore(ci): update xcode version support

--- tools/setup_osx_compiler.sh
+++ tools/setup_osx_compiler.sh
@@ -4,6 +4,10 @@
 COMPILER=$1
 
 # See Github hosted runners:
+# macos-14: https://github.com/actions/runner-images/blob/main/images/macos/macos-14-arm64-Readme.md
+#   xcode 14.3.1, 15.0.1, 15.1, 15.2, 15.3, 15.4, 16.0
+# macos-13: https://github.com/actions/runner-images/blob/main/images/macos/macos-13-Readme.md
+#   xcode 14.1, 14.2, 14.3.1, 15.0.1, 15.1, 15.2
 # macos-12: https://github.com/actions/runner-images/blob/main/images/macos/macos-12-Readme.md
 #   xcode 13.1, 12.2.1, 13.3.1, 13.4.1, 14.1, 14.2
 # maxos-11: https://github.com/actions/runner-images/blob/main/images/macos/macos-11-Readme.md
@@ -23,6 +27,10 @@ elif [[ $COMPILER == xcode13 ]]; then
     XCODE_PATH="/Applications/Xcode_13.2.1.app"
 elif [[ $COMPILER == xcode14 ]]; then
     XCODE_PATH="/Applications/Xcode_14.2.app"
+elif [[ $COMPILER == xcode15 ]]; then
+    XCODE_PATH="/Applications/Xcode_15.2.app"
+elif [[ $COMPILER == xcode16 ]]; then
+    XCODE_PATH="/Applications/Xcode_16.0.app"
 fi
 
 # Select our XCode version
